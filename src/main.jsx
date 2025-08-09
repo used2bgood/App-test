85d658a306a98f65ee3bd1c37b54a30a6ebba07e
used2bgood <jarenbanks@gmail.com>
2025-08-08 22:54:54 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
+// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      {/* Normal app (keep any existing auth/redirects you have) */}
+      {/* Normal app entry (keep whatever auth you have elsewhere) */}
       <Route path="/" element={<App />} />
-
-      {/* NEW: public route for iframe embed */}
+      {/* Public embed route */}
       <Route path="/embed" element={<App embed />} />
     </Routes>
   </BrowserRouter>
