d0aa566404e79f7b181ed5703ac7fafab2843c20
used2bgood <jarenbanks@gmail.com>
2025-08-08 23:21:49 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,6 @@
-// src/App.jsx
-import './App.css'
-import Pages from "@/pages/index.jsx"
-import { Toaster } from "@/components/ui/toaster"
-import { useLocation } from 'react-router-dom'
-
+import { useLocation } from 'react-router-dom';
 function App({ embed = false }) {
-  // belt & suspenders: also detect by URL
-  const { pathname } = useLocation()
-  const isEmbed = embed || pathname.startsWith('/embed')
-
-  return (
-    <>
-      <Pages embed={isEmbed} />
-      <Toaster />
-    </>
-  )
+  const { pathname } = useLocation();
+  const isEmbed = embed || pathname.startsWith('/embed');
+  return <Pages embed={isEmbed} />;
 }
-export default App
