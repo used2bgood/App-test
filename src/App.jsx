0e54fb1c2aef20837edce232d0fd29d7d692e8fa
used2bgood <jarenbanks@gmail.com>
2025-08-08 22:55:28 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
+// src/App.jsx
 import './App.css'
 import Pages from "@/pages/index.jsx"
 import { Toaster } from "@/components/ui/toaster"
 import { useLocation } from 'react-router-dom'
 
 function App({ embed = false }) {
-  // belt-and-suspenders: also detect via URL
-  const { pathname } = useLocation?.() || { pathname: window.location.pathname }
+  // belt & suspenders: also detect by URL
+  const { pathname } = useLocation()
   const isEmbed = embed || pathname.startsWith('/embed')
 
-  // You can pass this down as a prop or via context
   return (
     <>
       <Pages embed={isEmbed} />
