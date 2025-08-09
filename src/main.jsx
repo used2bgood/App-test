89eff6d38f09c2f2d3c2917a7d01a72f2a28e4ad
used2bgood <jarenbanks@gmail.com>
2025-08-08 23:21:07 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,6 @@
-// src/main.jsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from '@/App.jsx'
-import '@/index.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      {/* Normal app entry (keep whatever auth you have elsewhere) */}
-      <Route path="/" element={<App />} />
-      {/* Public embed route */}
-      <Route path="/embed" element={<App embed />} />
-    </Routes>
-  </BrowserRouter>
-)
+import { useLocation } from 'react-router-dom';
+function App({ embed = false }) {
+  const { pathname } = useLocation();
+  const isEmbed = embed || pathname.startsWith('/embed');
+  return <Pages embed={isEmbed} />;
+}
