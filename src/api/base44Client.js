5c944d2bea9809c740c394f0d0889e5f98e65bcc
used2bgood <jarenbanks@gmail.com>
2025-08-08 23:20:44 -0500

    Update base44Client.js

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -1,6 +1,5 @@
 import { createClient } from '@base44/sdk';
 
-// Detect "embed mode" by URL (works in dev and prod)
 const isEmbed =
   typeof window !== 'undefined' &&
   window.location &&
@@ -8,9 +7,5 @@ const isEmbed =
 
 export const base44 = createClient({
   appId: "689668dcb09ff1d7f1a2ff4d",
-  // In embed mode, do NOT require auth (prevents redirect to Base44)
   requiresAuth: !isEmbed,
-  // If your SDK supports it and you previously set a sign-in URL,
-  // you could also disable it in embed mode:
-  // signInUrl: isEmbed ? undefined : 'https://…/login'
 });
