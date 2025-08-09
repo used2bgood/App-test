d17ba707f167c2dfbfad61934a7d5678730a1b5e
used2bgood <jarenbanks@gmail.com>
2025-08-08 23:55:29 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '@/App.jsx'
+import '@/index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ErrorBoundary from './ErrorBoundary.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/embed" element={<App embed />} />
-    </Routes>
-  </BrowserRouter>
+  <ErrorBoundary>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/embed" element={<App embed />} />
+      </Routes>
+    </BrowserRouter>
+  </ErrorBoundary>
 );
